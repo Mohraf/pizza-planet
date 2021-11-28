41d44703478b90b49da10092d1987210c988a5a4
mohraf <amosngok@gmail.com>
2021-11-28 22:39:49 +0300

    Create Order Constructer

---
+++ static/js/main.js
@@ -0,0 +1,7 @@
+const Order = (size, crust, toppings, quantity, deliveryCost) => {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+  this.quantity = quantity;
+  this.deliveryCost = deliveryCost;
+}
