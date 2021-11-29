9fad13bef781f4085e7877bd3bb39e4530af4ebd
mohraf <amosngok@gmail.com>
2021-11-29 05:12:04 +0300

    Create Toppings Objects

--- static/js/main.js
+++ static/js/main.js
@@ -4,4 +4,25 @@ const Order = (size, crust, toppings, quantity, deliveryCost) => {
   this.toppings = toppings;
   this.quantity = quantity;
   this.deliveryCost = deliveryCost;
-}
+};
+
+const toppingsSmall = {
+  mushroom: 70,
+  bacon: 80,
+  sausage: 80,
+  brocolli: 60,
+};
+
+const toppingsMedium = {
+  mushroom: 80,
+  bacon: 90,
+  sausage: 90,
+  brocolli: 70,
+};
+
+const toppingsLarge = {
+  mushroom: 90,
+  bacon: 100,
+  sausage: 100,
+  brocolli: 80,
+};
