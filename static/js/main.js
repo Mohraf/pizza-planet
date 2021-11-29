e943081f219f611016da1b7636d66d471a04ada1
mohraf <amosngok@gmail.com>
2021-11-29 05:18:23 +0300

    Add prices object

--- static/js/main.js
+++ static/js/main.js
@@ -10,19 +10,25 @@ const toppingsSmall = {
   mushroom: 70,
   bacon: 80,
   sausage: 80,
-  brocolli: 60,
+  brocolli: 60
 };
 
 const toppingsMedium = {
   mushroom: 80,
   bacon: 90,
   sausage: 90,
-  brocolli: 70,
+  brocolli: 70
 };
 
 const toppingsLarge = {
   mushroom: 90,
   bacon: 100,
   sausage: 100,
-  brocolli: 80,
+  brocolli: 80
+};
+
+const pizzaPrices = {
+  small: 600,
+  medium: 800,
+  large: 1000
 };
